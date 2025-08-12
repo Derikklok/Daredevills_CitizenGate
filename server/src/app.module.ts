d0a76eb7d2861adb81274e8ed51ca2fdb3f7b8ec
Sachin <145639140+Derikklok@users.noreply.github.com>
2025-08-13 00:38:05 +0530

    feat: integrate TypeORM with PostgreSQL and add SystemAdmin module
    - Configured TypeORM to connect to a PostgreSQL database.
    - Added SystemAdmin module with controller and service.
    - Implemented a basic GET endpoint in BookingsController to retrieve all bookings.
    - Created entity for SystemAdmin with a unique username field.
    - Added unit tests for SystemAdminController and SystemAdminService.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookingsModule } from './bookings/bookings.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SystemAdminModule } from './system-admin/system-admin.module';
 
 @Module({
-  imports: [BookingsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url:'postgresql://postgres:iamironman0516@db.qntlhmmsysgawoelgkwa.supabase.co:5432/postgres',
+      synchronize: true, // ⚠️ Use only in dev
+      autoLoadEntities: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }) ,BookingsModule, SystemAdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/bookings/bookings.controller.ts
+++ server/src/bookings/bookings.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('bookings')
-export class BookingsController {}
+export class BookingsController {
+
+    @Get()
+    getAllBookings():string{
+        return 'This action returns all bookings';
+    }
+}
