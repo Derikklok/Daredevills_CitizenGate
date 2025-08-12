9f7629cd7cbeae9ec5bba49c1cba1722424493c6
nimeshaperi <112054998+nimeshaperi@users.noreply.github.com>
2025-08-12 21:57:27 +0530

    initial addition of both nestjs server and vite client

---
+++ server/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BookingController } from './booking/booking.controller';
+import { BookingModule } from './booking/booking.module';
+
+@Module({
+  imports: [BookingModule],
+  controllers: [AppController, BookingController],
+  providers: [AppService],
+})
+export class AppModule {}
