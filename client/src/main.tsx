da13e3b8208221c9f2cf176eb5f2614e21b088b0
Tihara <tiharaeg@gmail.com>
2025-08-13 20:33:42 +0530

    feat: setup react router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,9 @@
-import {StrictMode} from "react";
-import {createRoot} from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import {ClerkProvider} from "@clerk/clerk-react";
+import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from "@clerk/clerk-react";
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -11,10 +12,13 @@ if (!PUBLISHABLE_KEY) {
 	throw new Error("Add your Clerk Publishable Key to the .env file");
 }
 
-createRoot(document.getElementById("root")!).render(
+const root = createRoot(document.getElementById("root")!);
+root.render(
 	<StrictMode>
 		<ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-			<App />
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
 		</ClerkProvider>
 	</StrictMode>
 );
