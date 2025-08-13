8357a83426d6d6cafec84d26f8a74180d95c17e3
Tihara <tiharaeg@gmail.com>
2025-08-13 20:33:42 +0530

    feat: setup react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,5 @@
-import {useState} from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Link, Route, Routes} from "react-router-dom";
 import "./App.css";
-import {Button} from "@/components/ui/button";
 import {
 	SignedIn,
 	SignedOut,
@@ -11,10 +8,10 @@ import {
 	OrganizationSwitcher,
 } from "@clerk/clerk-react";
 import {AuthTest} from "./components/AuthTest";
+import BookingAppointments from "./pages/BookingAppointments";
+import MyAppointments from "./pages/MyAppointments";
 
-function App() {
-	const [count, setCount] = useState(0);
-
+function Home() {
 	return (
 		<>
 			<header>
@@ -26,33 +23,29 @@ function App() {
 					<OrganizationSwitcher />
 				</SignedIn>
 			</header>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-			<Button>Click me</Button>
 
-			{/* Auth Test Component */}
-			<SignedIn>
-				<AuthTest />
-			</SignedIn>
+			    {/* Auth Test Component */}
+				<SignedIn>
+					<AuthTest />
+				</SignedIn>
+
+				{/* Navigation Links */}
+				<nav>
+					<Link to="/booking-appointments">Booking Appointments</Link> |{" "}
+					<Link to="/my-appointments">My Appointments</Link>
+				</nav>
 		</>
+	)
+}
+
+
+function App() {
+	return (
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/booking-appointments" element={<BookingAppointments />} />
+				<Route path="/my-appointments" element={<MyAppointments />} />
+			</Routes>
 	);
 }
 
