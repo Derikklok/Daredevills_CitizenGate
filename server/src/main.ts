95e7b421074b579d5dda1b848de1e2641cb79abd
nimeshaperi <112054998+nimeshaperi@users.noreply.github.com>
2025-08-13 18:05:23 +0530

    removed swagger comment

--- server/src/main.ts
+++ server/src/main.ts
@@ -23,8 +23,8 @@ async function bootstrap() {
     .setVersion("1.0")
     .build();
 
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup("api-docs", app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api-docs", app, document);
 
   // Start the server
   await app.listen(process.env.PORT ?? 3000);
