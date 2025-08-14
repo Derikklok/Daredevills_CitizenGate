6149851ab26fafb95153cf813f633b7c1a5237a5
nimeshaperi <112054998+nimeshaperi@users.noreply.github.com>
2025-08-14 22:06:01 +0530

    added a layout structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,42 +1,12 @@
-import {Link, Route, Routes, Navigate} from "react-router-dom";
+import {Route, Routes, Navigate} from "react-router-dom";
 import "./App.css";
-import {
-	SignedIn,
-	SignedOut,
-	UserButton,
-	OrganizationSwitcher,
-	useAuth,
-} from "@clerk/clerk-react";
+import {useAuth} from "@clerk/clerk-react";
 import BookingAppointments from "./pages/BookingAppointments";
 import MyAppointments from "./pages/MyAppointments";
-import {Button} from "./components/ui/button";
 import SignInPage from "./pages/SignIn";
 import SignUpPage from "./pages/SignUp";
-
-function Home() {
-	return (
-		<>
-			<header>
-				<SignedOut>
-					<Link to="/sign-in">Sign In</Link> |{" "}
-					<Link to="/sign-up">Sign Up</Link>
-				</SignedOut>
-				<SignedIn>
-					<UserButton />
-					<OrganizationSwitcher />
-				</SignedIn>
-			</header>
-
-			<Button className="bg-primary-500">Click me</Button>
-
-			{/* Navigation Links */}
-			<nav>
-				<Link to="/booking-appointments">Booking Appointments</Link> |{" "}
-				<Link to="/my-appointments">My Appointments</Link>
-			</nav>
-		</>
-	);
-}
+import Home from "./pages/Home";
+import Layout from "./components/common/Layout";
 
 function App() {
 	const {isSignedIn, isLoaded} = useAuth();
@@ -48,17 +18,19 @@ function App() {
 
 	return (
 		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/booking-appointments" element={<BookingAppointments />} />
-			<Route path="/my-appointments" element={<MyAppointments />} />
-			<Route
-				path="/sign-in"
-				element={isSignedIn ? <Navigate to="/" replace /> : <SignInPage />}
-			/>
-			<Route
-				path="/sign-up"
-				element={isSignedIn ? <Navigate to="/" replace /> : <SignUpPage />}
-			/>
+			<Route path="/" element={<Layout />}>
+				<Route index element={<Home />} />
+				<Route path="/booking-appointments" element={<BookingAppointments />} />
+				<Route path="/my-appointments" element={<MyAppointments />} />
+				<Route
+					path="/sign-in"
+					element={isSignedIn ? <Navigate to="/" replace /> : <SignInPage />}
+				/>
+				<Route
+					path="/sign-up"
+					element={isSignedIn ? <Navigate to="/" replace /> : <SignUpPage />}
+				/>
+			</Route>
 		</Routes>
 	);
 }
